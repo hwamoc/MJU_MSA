45bddc99df1e29597b485650a242a1cb98f78d5d
Hwamoc <hwamok.mocha@gmail.com>
2019-10-25 14:08:08 +0900

    eureka-client not found error fixed

--- SpringBootPractice-Retaurant-v2/src/main/java/com/springboot/demo/ApiApplication.java
+++ SpringBootPractice-Retaurant-v2/src/main/java/com/springboot/demo/ApiApplication.java
@@ -4,12 +4,15 @@ import java.nio.charset.Charset;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+
 @SpringBootApplication
+@EnableEurekaClient
 public class ApiApplication {
 
 	public static void main(String[] args) {
