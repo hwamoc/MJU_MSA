a642e97e1f4274a6337d96aacea9a31837808c5a
Hwamoc <hwamok.mocha@gmail.com>
2019-10-25 10:33:12 +0900

    Fixed untracked files

--- SpringBootPractice-Retaurant/src/main/java/com/springboot/demo/repo/RestaurantJpaRepo.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.demo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springboot.demo.entity.Restaurant;
-
-public interface RestaurantJpaRepo extends JpaRepository<Restaurant, Integer> {
-//	Restaurant findByName(String restaurantName);
-}
