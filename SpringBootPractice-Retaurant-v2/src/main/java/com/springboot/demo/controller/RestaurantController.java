32c7029ca5fe4be15bdcf7e0c003e4fe84d85d3c
Hwamoc <hwamok.mocha@gmail.com>
2019-10-31 14:12:07 +0900

    request 한글 깨짐 오류 해결

--- SpringBootPractice-Retaurant-v2/src/main/java/com/springboot/demo/controller/RestaurantController.java
+++ SpringBootPractice-Retaurant-v2/src/main/java/com/springboot/demo/controller/RestaurantController.java
@@ -2,13 +2,16 @@ package com.springboot.demo.controller;
 
 import java.io.UnsupportedEncodingException;
 import java.util.List;
+import java.util.Optional;
 
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.google.gson.Gson;
 import com.springboot.demo.entity.Restaurant;
 import com.springboot.demo.model.response.CommonResult;
-import com.springboot.demo.model.response.ListResult;
 import com.springboot.demo.model.response.SingleResult;
 import com.springboot.demo.repo.RestaurantJpaRepo;
 import com.springboot.demo.service.ResponseService;
@@ -35,57 +37,70 @@ public class RestaurantController {
 	private final ResponseService responseService; // 결과를 처리할 Service
 
 	@ApiOperation(value = "식당 조회", notes = "모든 식당을 조회한다")
-	@GetMapping(value = "/restaurants", produces = "text/json; charset=UTF-8")
-	public String findAllRestaurant() {
+	@GetMapping(value = "/restaurants")
+	public ResponseEntity<List<Restaurant>> findAllRestaurant() {
 		// 결과데이터가 여러건인경우 getListResult를 이용해서 결과를 출력한다.
-		ListResult<Restaurant> sampleList = responseService.getListResult(restaurantJpaRepo.findAll());
+//		ListResult<Restaurant> sampleList = responseService.getListResult(restaurantJpaRepo.findAll());
 		
-		Gson gson = new Gson(); // Gson 사용
-	    return gson.toJson(sampleList); // json으로 변환 후, 리턴
+		List<Restaurant> restaurantList = restaurantJpaRepo.findAll();
+		return new ResponseEntity<List<Restaurant>>(restaurantList, HttpStatus.OK);
+//		Gson gson = new Gson(); // Gson 사용
+//	    return gson.toJson(sampleList); // json으로 변환 후, 리턴
 	}
 	
 
-	@ApiOperation(value = "식당 하나 조회", notes = "res_index로 식당을 조회한다")
-	@GetMapping(value = "/restaurant/{res_index}", produces = "text/json; charset=UTF-8")
-	public String findUserById(
-			@ApiParam(value = "식당ID", required = true) @PathVariable int res_index) {
+//	@ApiOperation(value = "식당 하나 조회", notes = "res_index로 식당을 조회한다")
+	@GetMapping(value = "/restaurant/{res_index}")
+//	public ResponseEntity<Restaurant> findUserById(
+//			@ApiParam(value = "식당ID", required = true) @PathVariable int res_index) {
 		// 결과데이터가 단일건인경우 getBasicResult를 이용해서 결과를 출력한다.
-		SingleResult<Restaurant> singleResult = responseService.getSingleResult(restaurantJpaRepo.findById(res_index).orElse(null));
-		Gson gson = new Gson(); // Gson 사용
-	    return gson.toJson(singleResult); // json으로 변환 후, 리턴
-	}
+//		SingleResult<Restaurant> singleResult = responseService.getSingleResult(restaurantJpaRepo.findById(res_index).orElse(null));
+//		Gson gson = new Gson(); // Gson 사용
+//	    return gson.toJson(singleResult); // json으로 변환 후, 리턴
+		
+//		Restaurant restaurant = restaurantJpaRepo.findById(res_index);
+//		return new ResponseEntity<Restaurant>(restaurant, HttpStatus.OK);
+//	}
 	
 	@ApiOperation(value = "식당 입력", notes = "식당을 입력한다.")
 	@PostMapping(value = "/restaurant")
-	public SingleResult<Restaurant> save(
-			@ApiParam(value = "식당 이름", required = true) @RequestParam String res_name,
-			@ApiParam(value = "식당 종류", required = true) @RequestParam String res_category,
-			@ApiParam(value = "식당 별점", required = false) @RequestParam Integer res_grade,
-			@ApiParam(value = "예상 대기 시간", required = true) @RequestParam Integer res_expected_minutes) throws UnsupportedEncodingException {
-		Restaurant restaurant = Restaurant.builder()
-				.res_name(new String(res_name.getBytes("8859_1"), "UTF-8"))
-				.res_category(new String(res_category.getBytes("8859_1"), "UTF-8"))
-				.res_grade(res_grade)
-				.res_expected_minutes(res_expected_minutes)
-				.build();
+	public ResponseEntity<Restaurant> save(
+//			@ApiParam(value = "식당 이름", required = true) @RequestParam String res_name,
+//			@ApiParam(value = "식당 종류", required = true) @RequestParam String res_category,
+//			@ApiParam(value = "식당 별점", required = false) @RequestParam Integer res_grade,
+//			@ApiParam(value = "예상 대기 시간", required = true) @RequestParam Integer res_expected_minutes
+			@RequestBody Restaurant restaurant
+			) throws UnsupportedEncodingException {
+//		Restaurant restaurant = Restaurant.builder()
+//				.res_name(new String(res_name.getBytes("8859_1"), "UTF-8"))
+//				.res_category(new String(res_category.getBytes("8859_1"), "UTF-8"))
+//				.res_grade(res_grade)
+//				.res_expected_minutes(res_expected_minutes)
+//				.build();
 		
-		return responseService.getSingleResult(restaurantJpaRepo.save(restaurant));
+//		return responseService.getSingleResult(restaurantJpaRepo.save(restaurant));
+		Restaurant newRestaurant = restaurantJpaRepo.save(restaurant);
+		return new ResponseEntity<Restaurant>(newRestaurant, HttpStatus.OK);
 	}
 	
 	@ApiOperation(value = "식당 수정", notes = "식당 정보를 수정한다")
     @PutMapping(value = "/restaurant/{res_index}")
-    public SingleResult<Restaurant> modify(
-    		@ApiParam(value = "식당 이름", required = true) @RequestParam String res_name,
-			@ApiParam(value = "식당 종류", required = true) @RequestParam String res_category,
-			@ApiParam(value = "식당 별점", required = false) @RequestParam Integer res_grade,
-			@ApiParam(value = "예상 대기 시간", required = true) @RequestParam Integer res_expected_minutes) throws UnsupportedEncodingException {
-		Restaurant restaurant = Restaurant.builder()
-				.res_name(new String(res_name.getBytes("8859_1"), "UTF-8"))
-				.res_category(new String(res_category.getBytes("8859_1"), "UTF-8"))
-				.res_grade(res_grade)
-				.res_expected_minutes(res_expected_minutes)
-				.build();
-		return responseService.getSingleResult(restaurantJpaRepo.save(restaurant));
+    public ResponseEntity<Restaurant> modify(
+//    		@ApiParam(value = "식당 이름", required = true) @RequestParam String res_name,
+//			@ApiParam(value = "식당 종류", required = true) @RequestParam String res_category,
+//			@ApiParam(value = "식당 별점", required = false) @RequestParam Integer res_grade,
+//			@ApiParam(value = "예상 대기 시간", required = true) @RequestParam Integer res_expected_minutes
+    		@RequestBody Restaurant restaurant) throws UnsupportedEncodingException {
+//		Restaurant restaurant = Restaurant.builder()
+//				.res_name(new String(res_name.getBytes("8859_1"), "UTF-8"))
+//				.res_category(new String(res_category.getBytes("8859_1"), "UTF-8"))
+//				.res_grade(res_grade)
+//				.res_expected_minutes(res_expected_minutes)
+//				.build();
+		
+//		return responseService.getSingleResult(restaurantJpaRepo.save(restaurant));
+		Restaurant modifiedRestaurant = restaurantJpaRepo.save(restaurant);
+		return new ResponseEntity<Restaurant>(modifiedRestaurant, HttpStatus.OK);
     }
 	
 	@ApiOperation(value = "식당 삭제", notes = "res_index로 식당 정보를 삭제한다")
