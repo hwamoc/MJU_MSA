f33126577e45489066eb41efc2121acca2cfdad2
Hwamoc <hwamok.mocha@gmail.com>
2019-11-08 14:08:31 +0900

    merge Restaurant

--- SpringBootPractice-Retaurant-v2/src/main/java/com/springboot/demo/entity/Menu.java
+++ SpringBootPractice-Retaurant-v2/src/main/java/com/springboot/demo/entity/Menu.java
@@ -13,11 +13,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Builder // builder를 사용할수 있게 합니다.
 @Entity // jpa entity임을 알립니다.
-@Setter
 @Getter // user 필드값의 getter를 자동으로 생성합니다.
 @NoArgsConstructor // 인자없는 생성자를 자동으로 생성합니다.
 @AllArgsConstructor // 인자를 모두 갖춘 생성자를 자동으로 생성합니다.
@@ -31,29 +29,28 @@ public class Menu {
 	@Column(nullable = true, length = 45) // uid column을 명시합니다. 필수이고 유니크한 필드이며 길이는 30입니다.
 	private int menu_price;
 	
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "res_index")
-    private Restaurant restaurant; // 메뉴 - 식당의 관계 - N:1
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "res_index")
+//    private Restaurant restaurant; // 메뉴 - 식당의 관계 - N:1
     
- // Join 테이블이 Json결과에 표시되지 않도록 처리.
-    protected Restaurant getRestaurant() {
-        return restaurant;
-    }
- 
-    // 생성자
-    public Menu(Restaurant restaurant, int menu_index, String menu_name, int menu_price) {
-    	this.restaurant = restaurant;
-    	this.menu_index = menu_index;
-        this.menu_name = menu_name;
-        this.menu_price = menu_price;
-    }
- 
-    // 수정시 데이터 처리
-    public Menu setUpdate(int menu_index, String menu_name, int menu_price) {
-    	this.menu_index = menu_index;
-        this.menu_name = menu_name;
-        this.menu_price = menu_price;
-        return this;
-    }
-
+// // Join 테이블이 Json결과에 표시되지 않도록 처리.
+//    protected Restaurant getRestaurant() {
+//        return restaurant;
+//    }
+// 
+//    // 생성자
+//    public Menu(Restaurant restaurant, int menu_index, String menu_name, int menu_price) {
+//    	this.restaurant = restaurant;
+//    	this.menu_index = menu_index;
+//        this.menu_name = menu_name;
+//        this.menu_price = menu_price;
+//    }
+// 
+//    // 수정시 데이터 처리
+//    public Menu setUpdate(int menu_index, String menu_name, int menu_price) {
+//    	this.menu_index = menu_index;
+//        this.menu_name = menu_name;
+//        this.menu_price = menu_price;
+//        return this;
+//    }
 }
--- SpringBootPractice-Retaurant-v2/src/main/java/com/springboot/demo/repo/GradeJpaRepo.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.demo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springboot.demo.entity.Grade;;
-
-public interface GradeJpaRepo extends JpaRepository<Grade, Integer> {
-//	Restaurant findByName(String restaurantName);
-}
